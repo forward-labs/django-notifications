d6f2e910679ecda2ac094f6c3ff278767e8f7f56
Abraham Alaka <abrahamalaka@campussociety.com>
2019-07-17 09:52:01 +0100

    migrations reference django jsonfield

--- notifications/migrations/0003_notification_data.py
+++ notifications/migrations/0003_notification_data.py
@@ -2,20 +2,18 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import jsonfield.fields
+from django.contrib.postgres.fields import JSONField
 
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('notifications', '0002_auto_20150224_1134'),
-    ]
+    dependencies = [("notifications", "0002_auto_20150224_1134")]
 
     operations = [
         migrations.AddField(
-            model_name='notification',
-            name='data',
-            field=jsonfield.fields.JSONField(null=True, blank=True),
+            model_name="notification",
+            name="data",
+            field=JSONField(null=True, blank=True),
             preserve_default=True,
-        ),
+        )
     ]
